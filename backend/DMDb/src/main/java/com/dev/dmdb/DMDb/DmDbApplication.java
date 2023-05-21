138bddc4f5db02cb19c7cd265609108c48016b34
DevSharma06 <deven2806@gmail.com>
2023-05-21 10:57:48 +0530

    Added initial files in frontend and backend

---
+++ backend/DMDb/src/main/java/com/dev/dmdb/DMDb/DmDbApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dmdb.DMDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmDbApplication.class, args);
+	}
+
+}
---
+++ backend/DMDb/src/test/java/com/dev/dmdb/DMDb/DmDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dmdb.DMDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
